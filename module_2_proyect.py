ee83bb47e0027920e6749d48f6d4402e5f6b9e39
Brayan Sanchez <brayansanchezm11z1@gmail.com>
2026-09-23 15:43:19 -0600

    Update name project

--- module_2_proyect.py
+++ module_2_proyect.py
@@ -1,23 +1,23 @@
 
-name = str(input("Enter the name of company: "))
+name = (input("Enter the name of company: "))
 time = int (input("Enter the time of use in seconds: "))
 rate = float(input("Enter the hourly price: "))
 
 h = time // 3600
 m = (time % 3600) // 60
-s = (time % 3600) % 60
+s = time % 60
 
-rate = (time / 3600) * rate
-iva = rate * 0.16
+subtotal = (time / 3600) * rate
+iva = subtotal * 0.16
 total = iva + rate
 
 print("=" * 40)
 print( "           SERVICE INVOICE")
 print("=" * 40)
-print("Client: ", '"'+ name + '"')
+print("Client:", '"'+ name + '"')
 print("Usage time: ",h, "h:",m, "m:",s, "s")
 
-print("\nSubtotal: " "$",rate)
+print("\nSubtotal: $",rate)
 print("IVA (16%): " "$", iva)
 print("-" * 40)
 print("TOTAL TO BE PAID: " "$",total)
